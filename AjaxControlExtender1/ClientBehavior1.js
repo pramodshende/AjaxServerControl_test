afe035217dd7c7b5bd735f263808746d06a09030
pramodshende <pramodshende@hotmail.com>
2016-04-29 10:51:22 -0400

    test ajax
    test ajax project

---
+++ AjaxControlExtender1/ClientBehavior1.js
@@ -0,0 +1,22 @@
+﻿/// <reference name="MicrosoftAjax.js"/>
+
+Type.registerNamespace("AjaxControlExtender1");
+
+AjaxControlExtender1.ClientBehavior1 = function(element) {
+    AjaxControlExtender1.ClientBehavior1.initializeBase(this, [element]);
+}
+
+AjaxControlExtender1.ClientBehavior1.prototype = {
+    initialize: function() {
+        AjaxControlExtender1.ClientBehavior1.callBaseMethod(this, 'initialize');
+        
+        // Add custom initialization here
+    },
+    dispose: function() {        
+        //Add custom dispose actions here
+        AjaxControlExtender1.ClientBehavior1.callBaseMethod(this, 'dispose');
+    }
+}
+AjaxControlExtender1.ClientBehavior1.registerClass('AjaxControlExtender1.ClientBehavior1', Sys.UI.Behavior);
+
+if (typeof(Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();
---
+++ AjaxServerControl1/ServerControl1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace AjaxServerControl1
+{
+    /// <summary>
+    /// Summary description for ServerControl1
+    /// </summary>
+    public class ServerControl1 : ScriptControl
+    {
+        public ServerControl1()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+      ///      Var Query =from CusDetails in db.customer skip (50) Take (10)
+    //ObjectDumper.Write(q)
+           // var qry = from cusD in DBNull. skip (50) take (10)
+
+        }
+        protected override IEnumerable<ScriptDescriptor>
+                GetScriptDescriptors()
+        {
+            ScriptControlDescriptor descriptor = new ScriptControlDescriptor("AjaxServerControl1.ClientControl1", this.ClientID);
+            yield return descriptor;
+        }
+
+        // Generate the script reference
+        protected override IEnumerable<ScriptReference>
+                GetScriptReferences()
+        {
+            yield return new ScriptReference("AjaxServerControl1.ClientControl1.js", this.GetType().Assembly.FullName);
+        }
+    
+    void abc()
+        {
+        var wordsA = new {"Rahul","ashok","sweta"};
+        var wordsB = new {"rahul","ashok","sweta"};
+        var mat = wordsA.sequenceequal(wordsB);
+
+        
+            Console.WriteLine("The sequences match: {0}", mat);
+
+
+        }
+    }
+}
